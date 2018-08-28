4ee141538f5fd5912303f8359a7d37d1c921bbef
dalice.chu <dalice157@gmail.com>
2018-08-28 13:29:53 +0800

    將webpack升級到4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import app from './App.css'
 import Button from 'ui/button/Button'
 import Select from 'ui/select/Select'
 import PageController from 'ui/pageController/PageController'
@@ -8,13 +7,9 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <div>
-          <PageController />
-        </div>
-        <div>
+        <PageController />
         <Button typeStyle="default">Click Me!!</Button>
         <Select />
-        </div>
       </React.Fragment>
     );
   }
